771a941f59fc1f6f9568c058988e23b697868bbc
jmvill01 <32886311+jmvill01@users.noreply.github.com>
2022-11-05 23:57:55 -0700

    Player-Controlled Flashlight

---
+++ Assets/Scripts/FlashFlickr.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashFlickr : MonoBehaviour
+{
+    private GameObject Flashlight;
+    private bool on = false;
+    private GameObject Rod;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi there");
+        Flashlight = GameObject.Find("Directional Light");
+
+        Rod = GameObject.Find("Cube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Flashlight);
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (on == false)
+            {
+                Rod.transform.localScale += new Vector3(0, 0, 5);
+                Flashlight.SetActive(true);
+            }
+            else
+            {
+                Rod.transform.localScale -= new Vector3(0, 0, 5);
+                Flashlight.SetActive(false);
+            }
+            on = !on;
+
+        }
+    }
+
+}
