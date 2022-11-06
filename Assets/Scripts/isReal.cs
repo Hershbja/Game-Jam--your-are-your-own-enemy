6e27d4d04195fefd23d10fe36cc22a2cef7da9aa
jmvill01 <32886311+jmvill01@users.noreply.github.com>
2022-11-05 21:28:10 -0700

    Destroys Walls with Light Interaction

--- Assets/Scripts/isReal.cs
+++ Assets/Scripts/isReal.cs
@@ -4,17 +4,5 @@ using UnityEngine;
 
 public class isReal : MonoBehaviour
 {
-
-    [SerializeField] private bool isRealProp;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] public bool isRealProp;
 }
