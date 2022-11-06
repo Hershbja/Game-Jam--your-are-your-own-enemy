a5d29e09c264c07de569cf39de895613fbe401b5
Hershbja <ander.the.enderwolf@gmail.com>
2022-11-06 00:56:51 -0700

    Update FlashFlickr.cs

--- Assets/Scripts/FlashFlickr.cs
+++ Assets/Scripts/FlashFlickr.cs
@@ -5,36 +5,54 @@ using UnityEngine;
 public class FlashFlickr : MonoBehaviour
 {
     private GameObject Flashlight;
+    private Light lightcomp;
     private bool on = false;
     private GameObject Rod;
+    private bool dead = false;
+    public float battery = 10f;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hi there");
         Flashlight = GameObject.Find("Directional Light");
+        lightcomp = Flashlight.GetComponentInChildren<Light>();
 
         Rod = GameObject.Find("Cube");
+        Flashlight.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Flashlight);
+        //Debug.Log(Flashlight);
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (on == false)
+            if (on == false && !dead)
             {
                 Rod.transform.localScale = new Vector3(0, 0, 7);
                 Flashlight.SetActive(true);
+                //Debug.Log("Flashlight On");
             }
             else
             {
                 Rod.transform.localScale = new Vector3(0, 0, 2);
                 Flashlight.SetActive(false);
+                //Debug.Log("Flashlight Off");
             }
             on = !on;
 
         }
+        if (on && !dead)
+        {
+            Debug.Log(battery);
+            battery -= Time.deltaTime;
+            lightcomp.intensity = 15-15/battery;
+            if(battery <= 0)
+            {
+                dead = true;
+                Rod.transform.localScale = new Vector3(0, 0, 2);
+                Flashlight.SetActive(false);
+            }
+        }
     }
-
 }
